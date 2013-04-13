f91b437f69caa749cfaaac2c45a431bb52d1d08c
=Mark Hudnall <hudnall.mark@gmail.com>
2013-04-13 02:56:48 -0700

    strip HTML tags

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,6 @@
 import nltk as nl
+from HTMLParser import HTMLParser
+
 # import httplib2
 # import beautifulsoup as bs
 stopwords = set()
@@ -9,9 +11,23 @@ with open('english_stopwords.txt') as f:
 stopwords |= set([",",";",":","?","=","+","."])
 
 def tokenize_title (title):
-	tokens = nl.tokenize.word_tokenize(title) #nl.tag.pos_tag(nl.tokenize.word_tokenize(title))
+	tokens = nl.tokenize.word_tokenize(strip_tags(title))
 	words = [w for w in tokens if w.lower() not in stopwords]
 	return words
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 # def has_good_punctuation(p):
