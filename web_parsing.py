5b1b801ff852550ada987a3a1a480380bb73b4b3
=Mark Hudnall <hudnall.mark@gmail.com>
2013-04-13 04:42:09 -0700

    use requests

--- web_parsing.py
+++ web_parsing.py
@@ -1,10 +1,12 @@
-import urllib
+import requests
 from readability.readability import Document
 # pip install readability-lxml
 from naive_bayes import strip_tags
 
 def get_extract(*args,**kwargs):
-	# loads webpage and gets content
-	html = urllib.urlopen(kwargs.get('url')).read()
-	readable_article = Document(html).summary()
-	return strip_tags(readable_article)
+    # loads webpage and gets content
+    url = kwargs.get('url')
+    print url
+    html = requests.get(url).text
+    readable_article = Document(html).summary()
+    return strip_tags(readable_article)
